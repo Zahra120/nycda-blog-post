20ad99ed2ba61e2fe2538473116622bda9179196
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-11-14 16:39:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+      pug = require('pug'),
+      morgan = require('morgan'),
+      bodyParser = require('body-parser'),
+      Sequelize = require('sequelize');
+
+
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+   res.render('index');
+});
+
+
+app.listen(3000, function(){
+   console.log('web server runs on port 3000');
+});
