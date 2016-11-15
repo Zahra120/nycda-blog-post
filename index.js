815616921c3800e9297ea28ff585050f1925f678
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-11-15 14:07:18 +0100

    delete works

--- index.js
+++ index.js
@@ -24,38 +24,49 @@ app.use(methodOverride(function(req, res){
 
 app.get('/admin/posts', (req, res) => {
    db.Blog.findAll().then((blogs) => {
-   res.render('posts/index', { blogs: blogs });
+      res.render('posts/index', { blogs: blogs });
    });
 });
 
 app.get('/admin/posts/new', (req,res) => {
    res.render('posts/new' );
 });
+
 app.get('/admin/posts/:id', (req, res) => {
    db.Blog.findById(req.params.id).then((blog) => {
-      res.render('posts/show',{ blog: blog });
+      res.render('posts/show', { blog: blog });
+   });
 });
 
-});
+
 app.post('/posts', (req, res) => {
    db.Blog.create(req.body).then(() => {
-   res.redirect('/admin/posts');
+      res.redirect('/admin/posts');
    });
 });
 
 app.put('/admin/posts/:id', (req, res) => {
    db.Blog.update(req.body, {
-    where: {
-      id: req.params.id
+      where: {
+         id: req.params.id
       }
     }).then(() => {
       res.redirect('/admin/posts/' + req.params.id);
    });
-
 });
+
 app.get('/admin/posts/edit/:id', (req, res) => {
    db.Blog.findById(req.params.id).then((blog) => {
       res.render('posts/edit', { blog: blog });
+   });
+});
+app.delete('/admin/posts/:id', (req, res) => {
+   db.Blog.destroy({
+      where: {
+         id: req.params.id
+      }
+   }).then(() => {
+      res.redirect('/admin/posts');
    });
 });
 
